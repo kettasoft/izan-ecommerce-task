75909d80954c3310e36ab96b68ec04ab7a5f193d
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-06-26 18:09:54 +0300

    test: handle test structure

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,26 @@
 
 namespace Tests;
 
+use Database\Seeders\DummyDatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-    //
+    use CreatesApplication, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run a specific seeder
+        $this->seed(DummyDatabaseSeeder::class);
+    }
+
+    public function runItemsManagement()
+    {
+        $this->seed(\Modules\Users\Database\Seeders\UsersDatabaseSeeder::class);
+        // $this->seed(\Modules\Products\Database\Seeders\ProductsDatabaseSeeder::class);
+        // $this->seed(\Modules\Orders\Database\Seeders\OrdersDatabaseSeeder::class);
+    }
 }
