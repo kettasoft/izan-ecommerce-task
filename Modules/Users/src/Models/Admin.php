9c07049132b4a983f6f90d0b6d3c3de7291f1b9d
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-06-28 17:50:06 +0300

    feat: add method to get class name for polymorphic relations.

--- Modules/Users/src/Models/Admin.php
+++ Modules/Users/src/Models/Admin.php
@@ -10,6 +10,16 @@ class Admin extends User
 {
     use HasFactory, HasParent;
 
+    /**
+     * Get the class name for polymorphic relations.
+     *
+     * @return string
+     */
+    public function getMorphClass()
+    {
+        return User::class;
+    }
+
     protected static function newFactory(): AdminFactory
     {
         return AdminFactory::new();
--- Modules/Users/src/Models/Customer.php
+++ Modules/Users/src/Models/Customer.php
@@ -3,6 +3,7 @@
 namespace Modules\Users\Models;
 
 use Parental\HasParent;
+use Modules\Users\Models\User;
 use Modules\Users\Database\Factories\CustomerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,5 +14,15 @@ class Customer extends User
     protected static function newFactory(): CustomerFactory
     {
         return CustomerFactory::new();
+    }
+
+    /**
+     * Get the class name for polymorphic relations.
+     *
+     * @return string
+     */
+    public function getMorphClass()
+    {
+        return User::class;
     }
 }
