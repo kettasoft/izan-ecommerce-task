4230c9855da24557084b59a587ed5da68ec021f7
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-06-28 17:54:03 +0300

    fix: replace middleware from auth:sanctum to web

--- Modules/Users/routes/api.php
+++ Modules/Users/routes/api.php
@@ -5,11 +5,11 @@ use Modules\Users\Http\Controllers\LogoutController;
 use Modules\Users\Http\Controllers\ProfileController;
 use Modules\Users\Http\Controllers\Auth\LoginController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['web'])->group(function () {
     Route::get('profile', ProfileController::class)->name('profile');
 });
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware(['web'])->group(function () {
     Route::post('login', LoginController::class)->name('login');
     Route::post('logout', LogoutController::class)->name('logout');
 });
